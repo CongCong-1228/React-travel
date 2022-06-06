a3b5deea3503020fcef66ff968b94a72c33f37b6
congcong1228 <fivesun1228@163.com>
2022-06-06 21:35:35 +0800

    add axios get api request

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './App';
 import 'antd/dist/antd.min.css';
-import store from '../src/redux/store'
+import store from '../src/redux/store';
 import {Provider} from "react-redux";
-import './i18n/config'
+import './i18n/config';
+import axios from "axios";
+
+axios.defaults.headers['x-icode'] = 'A9D80AA716CE595E';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,72 @@
 import React from 'react'
 import styles from './index.module.css'
 import {Header, Footer, SideMenu, Carousel, ProductList, BusinessPartners} from '../../components'
-import {Row, Col, Typography} from 'antd'
-import {productList1, productList2, productList3} from '../../mock'
+import {Row, Col, Typography, Spin} from 'antd'
+// import {productList1, productList2, productList3} from '../../mock'
 import slider1 from '../../assets/images/sider_2019_12-09.png'
 import slider2 from '../../assets/images/sider_2019_02-04-2.png'
 import slider3 from '../../assets/images/sider_2019_02-04.png'
 import {withTranslation, WithTranslation} from "react-i18next";
 import {connect} from "react-redux";
+import axios from "axios";
 
-class HomePage extends React.Component<WithTranslation> {
+interface State {
+    productList: any[],
+    loading: boolean,
+    error: string | null,
+}
+
+
+class HomePage extends React.Component<WithTranslation, State> {
     constructor(props) {
         super(props);
+        this.state = {
+            productList: [],
+            loading: true,
+            error: null,
+        }
+    }
+
+    getData = async () => {
+        try {
+            const response = await axios.get('http://123.56.149.216:8080/api/productCollections')
+            this.setState({
+                productList: response.data,
+                loading: false,
+            })
+        } catch (error: any) {
+            this.setState({
+                error: error.message,
+                loading: false
+            })
+        }
+
+    }
+
+    componentDidMount() {
+        this.getData();
     }
 
     render() {
         const {t} = this.props
+        const {productList, loading, error} = this.state
+        if (loading) {
+            return (
+                <Spin size="large"
+                      style={{
+                          height: '100vh',
+                          // width: 200,
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center'
+                      }}/>
+            )
+        }
+
+        if (error) {
+            return <span>{error}</span>
+        }
+
         return (
             <>
                 <Header/>
@@ -32,17 +83,19 @@ class HomePage extends React.Component<WithTranslation> {
                         title={<Typography.Title type='warning'
                                                  level={3}>{t("home_page.hot_recommended")}</Typography.Title>}
                         sliderImage={slider1}
-                        productions={productList1}
+                        productions={productList[0].touristRoutes}
                     />
                     <ProductList
-                        title={<Typography.Title type='danger' level={3}>{t("home_page.new_arrival")}</Typography.Title>}
+                        title={<Typography.Title type='danger'
+                                                 level={3}>{t("home_page.new_arrival")}</Typography.Title>}
                         sliderImage={slider2}
-                        productions={productList2}
+                        productions={productList[1].touristRoutes}
                     />
                     <ProductList
-                        title={<Typography.Title type='success' level={3}>{t("home_page.domestic_travel")}</Typography.Title>}
+                        title={<Typography.Title type='success'
+                                                 level={3}>{t("home_page.domestic_travel")}</Typography.Title>}
                         sliderImage={slider3}
-                        productions={productList3}
+                        productions={productList[2].touristRoutes}
                     />
 
                 </div>
